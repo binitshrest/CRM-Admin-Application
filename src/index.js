ef41281a9cd98e875ce6f9095031a29d170008f9
Binit Shrestha <53391715+binitshrest@users.noreply.github.com>
2024-02-10 19:25:24 +0530

    added extra functionality and context providers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ContextProvider } from "./contexts/ContextProvider";
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<App />);
 
 ReactDOM.render(
-  <App />,
+  <ContextProvider>
+      <App />
+  </ContextProvider>
+  ,
 
   document.getElementById("root")
 );
